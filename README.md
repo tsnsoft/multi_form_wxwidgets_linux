eb1c4f1d0cbff2bc4d62bb51ac495fe39cefacdc
proffix4 <proffix4@gmail.com>
2020-03-13 20:14:35 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# multi_form_wxwidgets_linux
+Пример многооконной программы на С++ в Code::Blocks (linux) с использованием wxFrame
+
+![Screenshot](screenshot1.jpg)
+
+![Screenshot](screenshot2.jpg)
+
+![Screenshot](screenshot3.jpg)
+
+![Screenshot](screenshot4.jpg)
+
+![Screenshot](screenshot5.png)
+
+
---
+++ multi_form_wxwidgetsApp.cpp
@@ -0,0 +1,35 @@
+/***************************************************************
+ * Name:      multi_form_wxwidgetsApp.cpp
+ * Purpose:   Code for Application Class
+ * Author:    Talipov S.N. (talipovsn@gmail.com)
+ * Created:   2020-03-06
+ * Copyright: Talipov S.N. (https://github.com/tsnsoft)
+ * License:
+ **************************************************************/
+
+#include "multi_form_wxwidgetsApp.h"
+
+//(*AppHeaders
+#include "multi_form_wxwidgetsMain.h"
+#include <wx/image.h>
+//*)
+
+IMPLEMENT_APP(multi_form_wxwidgetsApp);
+
+bool multi_form_wxwidgetsApp::OnInit()
+{
+    //(*AppInitialize
+    bool wxsOK = true;
+    wxLocale m_locale;
+    wxInitAllImageHandlers();
+    if ( wxsOK )
+    {
+    	m_locale.Init(wxLANGUAGE_RUSSIAN);
+    	multi_form_wxwidgetsFrame* Frame = new multi_form_wxwidgetsFrame(0);
+    	Frame->Show();
+    	SetTopWindow(Frame);
+    }
+    //*)
+    return wxsOK;
+
+}
